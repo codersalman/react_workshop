bdd2666907938722b08e411640be127268e220ac
Salman Shaikh <coder88salman@gmail.com>
2024-09-16 19:30:39 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-import StudentList from './components/StudentList';
-import ReactState from "./components/ReactState";
+
 import Clock from "./components/Clock";
-import ServerConnecttion from "./components/ServerConnecttion";
 
 function App() {
 
